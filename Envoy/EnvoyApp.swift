6927bbffb47f31bf4e84c775fbf7630d0558ce62
Suman Raj Sharma <sumanrajsharma2014@gmail.com>
2025-12-06 00:11:21 +1100

    Initial Commit

---
+++ Envoy/EnvoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvoyApp.swift
+//  Envoy
+//
+//  Created by Suman Raj Sharma on 6/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct EnvoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
